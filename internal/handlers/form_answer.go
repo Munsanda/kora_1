a886d4b44820957abb37462cfe920e28debdb18c
Chiwende <chiwendesakala@gmail.com>
2026-01-22 09:40:59 +0200

    Implement CreateQuestionHandler and define QuestionRequest struct

---
+++ internal/handlers/form_answer.go
@@ -0,0 +1,5 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func SubmitFormHandler(c *gin.Context) {}
---
+++ internal/handlers/questions_handler.go
@@ -0,0 +1,10 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+type QuestionRequest struct {
+}
+
+func CreateQuestionHandler(c *gin.Context) {
+
+}
